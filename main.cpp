d5251e72aebe0b27bb6bae26d94bc75f65b5f6ea
Juan Jose Castellanos <juan.castellanos@smartmatic.com>
2020-04-12 13:25:34 -0500

    add example for structured binding in range-based for loop

--- main.cpp
+++ main.cpp
@@ -1,8 +1,16 @@
-#include <iostream>
 #include <functional>
+#include <iostream>
+#include <map>
 
 int main()
 {
-  [out = std::ref(std::cout << "Hello ")](){out.get() << "World\n";}();
-}
+    [out = std::ref(std::cout << "Hello ")]() { out.get() << "World\n"; }();
 
+    std::map<std::uint32_t, std::string_view> m{{1, "one"}, {2, "two"}, {3, "three"}};
+
+    //Structured binding
+    for (const auto& [key, value] : m)
+    {
+        std::cout << "Key: " << key << " Value: " << value << "\n";
+    }
+}
