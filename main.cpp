3f3ab5cdc713b1633360857a8bc60a4f0ca1957a
Juan Jose Castellanos <juan.castellanos@smartmatic.com>
2020-07-08 19:25:40 -0500

    update format spaces to 2

--- main.cpp
+++ main.cpp
@@ -4,13 +4,13 @@
 
 int main()
 {
-    [out = std::ref(std::cout << "Hello ")]() { out.get() << "World\n"; }();
+  [out = std::ref(std::cout << "Hello ")]() { out.get() << "World\n"; }();
 
-    std::map<std::uint32_t, std::string_view> m{{1, "one"}, {2, "two"}, {3, "three"}};
+  std::map<std::uint32_t, std::string_view> m{{1, "one"}, {2, "two"}, {3, "three"}};
 
-    //Structured binding
-    for (const auto& [key, value] : m)
-    {
-        std::cout << "Key: " << key << " Value: " << value << "\n";
-    }
+  //Structured binding
+  for (const auto& [key, value] : m)
+  {
+    std::cout << "Key: " << key << " Value: " << value << "\n";
+  }
 }
